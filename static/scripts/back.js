8ee3e905af2c0dd544f52975e9d84271913ef5d0
Exponential182 <mthomas4773@gmail.com>
2026-07-29 14:01:37 +1200

    Made back button on error pages only appear when the last page was the website.

--- static/scripts/back.js
+++ static/scripts/back.js
@@ -1,7 +1,16 @@
-back_button = document.getElementById("back-button");
+const back_button = document.getElementById("back-button");
+const seperator = document.getElementById("error-button-seperator")
+const referrer = document.referrer
+const self = window.location.origin
+
+if (referrer.startsWith(self) != true) {
+    back_button.disabled = true
+    back_button.style.display = "none"
+    seperator.style.display = "none"
+}
 
 back_button.addEventListener("click", () => {
-    if (window.history.length > 1) {
+    if (referrer.startsWith(self)) {
         history.back()
     }
 });
