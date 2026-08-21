b3dd33bea0133bafc6d201f1123c642c38d5a893
Exponential182 <147586495+Exponential182@users.noreply.github.com>
2026-08-21 16:33:07 +1200

    Updated databse schema and change download and iframes to adapt
     - Moved files from GameFile to Game

--- models.py
+++ models.py
@@ -15,7 +15,12 @@ class Game(Base):
     name: Mapped[str] = mapped_column()
     tagline: Mapped[str] = mapped_column()
     description: Mapped[str] = mapped_column()
+    html_game_path: Mapped[str] = mapped_column()
     has_html: Mapped[bool] = mapped_column()
+    download_game_path: Mapped[str] = mapped_column()
+    has_windows: Mapped[bool] = mapped_column()
+    has_mac: Mapped[bool] = mapped_column()
+    has_linux: Mapped[bool] = mapped_column()
     cover_image_url: Mapped[str] = mapped_column()
     visibility: Mapped[bool] = mapped_column()
     dev_state: Mapped[str] = mapped_column()
@@ -24,7 +29,6 @@ class Game(Base):
     # One to Many Links
     genre_id: Mapped[int] = mapped_column(ForeignKey("Genre.id"))
     genre: Mapped["Genre"] = relationship(back_populates="games")
-    files: Mapped[list["GameFile"]] = relationship()
 
     # Many to Many Link
     users: Mapped[list["User"]] = relationship(
@@ -59,19 +63,6 @@ class Genre(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column()
     games: Mapped[list["Game"]] = relationship(back_populates="genre")
-
-
-class GameFile(Base):
-    """Represents a game file."""
-
-    __tablename__ = "GameFile"
-    file_id: Mapped[int] = mapped_column(primary_key=True)
-    game_id: Mapped[int] = mapped_column(ForeignKey("Game.id"))
-    path: Mapped[str] = mapped_column()
-    is_html5: Mapped[bool] = mapped_column()
-    is_windows: Mapped[bool] = mapped_column()
-    is_mac: Mapped[bool] = mapped_column()
-    is_linux: Mapped[bool] = mapped_column()
 
 
 class UserGame(Base):
